3dba598183cb4e375c26417fef0a33d31040c9db
swaminathan <swaminathankannank@gmail.com>
2025-02-06 23:11:45 +0530

    padding fix

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './top-bar.component.html',
-  styleUrl: './top-bar.component.css'
+  styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
+  
+  constructor(private router: Router) {}
 
+  home(value: string) {
+    console.log(value)
+    this.router.navigate([value]);
+  }
 }
