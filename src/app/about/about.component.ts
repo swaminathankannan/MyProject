dea5759eb4d3a83ccd76c8b7a97d66b81df7bd7d
swaminathan <swaminathankannank@gmail.com>
2025-02-19 21:10:26 +0530

    add download cv btn

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectorRef, Component, Inject, OnInit, AfterViewInit, PLATFORM_ID } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnInit,
+  AfterViewInit,
+  PLATFORM_ID,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -12,8 +21,8 @@ export class AboutComponent implements OnInit, AfterViewInit {
   activeSlide: number = 1;
   totalSlides: number = 2;
   carousel: any;
-  carouselMobile: any;
   carouselItems: any[] = [];
+  @ViewChild('downloadbtn') downloadBtn!: ElementRef;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -35,25 +44,11 @@ export class AboutComponent implements OnInit, AfterViewInit {
     if (carouselElement) {
       this.carousel = new (window as any).bootstrap.Carousel(carouselElement, {
         interval: false, // Disable auto sliding
-        ride: false, 
+        ride: false,
         wrap: false, // Prevent infinite looping
       });
 
       carouselElement.addEventListener('slid.bs.carousel', (event: any) => {
-        this.activeSlide = event.to + 1;
-        this.cdr.detectChanges();
-      });
-    }
-
-    const carouselElementMobile = document.getElementById('carouselMobile');
-    if (carouselElementMobile) {
-      this.carouselMobile = new (window as any).bootstrap.Carousel(carouselElementMobile, {
-        interval: false,
-        ride: false,
-        wrap: false,
-      });
-
-      carouselElementMobile.addEventListener('slid.bs.carousel', (event: any) => {
         this.activeSlide = event.to + 1;
         this.cdr.detectChanges();
       });
@@ -64,11 +59,13 @@ export class AboutComponent implements OnInit, AfterViewInit {
     this.carouselItems = [
       {
         title: 'Education 2019-2022 And 2023-2025',
-        description: 'Completed BSc Computer Science at National College Trichy followed by corress MSc in Computer Science at Pioneer arts and science college in Bharathidasan University',
+        description:
+          'Completed BSc Computer Science at National College Trichy followed by corress MSc in Computer Science at Pioneer arts and science college in Bharathidasan University',
       },
       {
         title: 'Experience',
-        description: 'As a dedicated Front-End Developer and Software Engineer at Niiv software private limited in Trichy with 2 years of experience, I am passionate about creating intuitive user experiences through modern web technologies. I am committed to continuous learning and staying current with industry trends.',
+        description:
+          'As a dedicated Front-End Developer and Software Engineer at Niiv software private limited in Trichy with 2 years of experience, I am passionate about creating intuitive user experiences through modern web technologies. I am committed to continuous learning and staying current with industry trends.',
       },
     ];
   }
@@ -78,10 +75,6 @@ export class AboutComponent implements OnInit, AfterViewInit {
       this.carousel.next();
       this.activeSlide = Math.min(this.activeSlide + 1, this.totalSlides);
     }
-    if (this.carouselMobile) {
-      this.carouselMobile.next();
-      this.activeSlide = Math.min(this.activeSlide + 1, this.totalSlides);
-    }
   }
 
   prevSlide() {
@@ -89,9 +82,14 @@ export class AboutComponent implements OnInit, AfterViewInit {
       this.carousel.prev();
       this.activeSlide = Math.max(this.activeSlide - 1, 1);
     }
-    if (this.carouselMobile) {
-      this.carouselMobile.prev();
-      this.activeSlide = Math.max(this.activeSlide - 1, 1);
+  }
+
+  downloadCv() {
+    if (this.downloadBtn) {
+      window.open(
+        'https://drive.google.com/file/d/1CQCPixNW1-pFyXcJvgTJRVFAAxTu6quT/view?usp=drivesdk',
+        '_blank'
+      );
     }
   }
 
